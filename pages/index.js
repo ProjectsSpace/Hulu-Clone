3976d99be639822c792b38c730a374494e0ebc15
Mocarram Hosan <mocarramhosan@gmail.com>
2021-10-11 18:43:26 +0600

    Landing page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 import Head from "next/head";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
       <Head>
         <title>Hulu Next</title>
         <meta name="description" content="Hulu next build" />
       </Head>
 
-      <main></main>
+      <main>
+        <Header />
+
+        <Nav />
+      </main>
 
       <footer></footer>
     </div>
