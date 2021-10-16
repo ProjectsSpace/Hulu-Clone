577e6d19a3c36a1e934fe75c89aeed52a3351bd7
Mocarram Hosan <mocarramhosan@gmail.com>
2021-10-16 23:57:53 +0600

    Header item hover state change

--- pages/components/utils/HeaderItem.js
+++ pages/components/utils/HeaderItem.js
@@ -2,7 +2,7 @@ const HeaderItem = ({ title, Icon }) => {
   return (
     <div className="flex flex-col items-center cursor-pointer group w-12 sm:px-10 hover:text-white">
       <Icon className="h-8 mb-1 group-hover:animate-bounce" />
-      <p className="tracking-widest uppercase hidden lg:inline-block">
+      <p className="tracking-widest uppercase hidden md:inline-block md:opacity-0 group-hover:opacity-100">
         {title}
       </p>
     </div>
