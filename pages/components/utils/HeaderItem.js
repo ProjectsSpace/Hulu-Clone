6a6278a5b723d2312e932582b822cd85bea88396
Mocarram Hosan <mocarramhosan@gmail.com>
2021-10-16 23:23:23 +0600

    Header design

--- pages/components/utils/HeaderItem.js
+++ pages/components/utils/HeaderItem.js
@@ -1,8 +1,8 @@
 const HeaderItem = ({ title, Icon }) => {
   return (
-    <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
-      <Icon className="h-8 mb-1 group-hover:animate-pulse" />
-      <p className="tracking-widest uppercase opacity-0 group-hover:opacity-100 ">
+    <div className="flex flex-col items-center cursor-pointer group w-12 sm:px-10 hover:text-white">
+      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+      <p className="tracking-widest uppercase hidden lg:inline-block">
         {title}
       </p>
     </div>
