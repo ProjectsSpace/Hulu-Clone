174271f2b7b327686b5557014be571bfaf4a0992
Mocarram Hosan <mocarramhosan@gmail.com>
2021-10-16 23:23:43 +0600

    Movie navigation

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -5,10 +5,10 @@ const Nav = () => {
   const router = useRouter();
   return (
     <nav className="relative">
-      <div className="flex gap-10 sm:gap-20 px-6 sm:px-16 text-2xl overflow-x-scroll scrollbar-hide">
+      <div className="flex gap-10 sm:gap-20 px-6 sm:px-16 text-2xl overflow-x-scroll scrollbar-hide justify-center">
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
-            className=" whitespace-nowrap cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
+            className="whitespace-nowrap cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
             key={key}
             onClick={() => router.push(`/?genre=${key}`)}
           >
